685b202dac19b6b9ab30e2864c72bdd3b8b74544
Maxwell Powell <powell.759@osu.edu>
2016-09-29 15:56:38 -0400

    added methods for merging series and parallel elements in a circuit

--- src/Circuit.java
+++ src/Circuit.java
@@ -13,5 +13,44 @@ public class Circuit {
 		this.elements = new HashSet<Element>();
 	}
 	
-
+	public void addNode(Node node){
+		this.nodes.add(node);
+	}
+	
+	private void removeNode(Node node){
+		this.nodes.remove(node);
+	}
+	
+	public void addElement(Element element){
+		this.elements.add(element);
+	}
+	
+	private void removeElement(Element element){
+		this.elements.remove(element);
+	}
+	
+	private void mergeSeries(Element one, Element two){
+		Node nodeOne = one.getNodeOne();
+		Node nodeTwo = two.getNodeTwo();
+		this.removeElement(one);
+		this.removeElement(two);
+		this.removeNode(one.getNodeTwo());
+		int sum = one.get_value() + two.get_value();
+		Element rEq = new Element(Element.Type.RESISTOR, sum, nodeOne, nodeTwo);
+		this.addElement(rEq);
+		
+	}
+	
+	private void mergeParallel(Element one, Element two){
+		Node nodeOne = one.getNodeOne();
+		Node nodeTwo = one.getNodeTwo();
+		this.removeElement(one);
+		this.removeElement(two);
+		int oneVal = one.get_value();
+		int twoVal = two.get_value();
+		int sum = oneVal * twoVal / (oneVal + twoVal);
+		Element rEq = new Element(Element.Type.RESISTOR, sum, nodeOne, nodeTwo);
+		this.addElement(rEq);
+		
+	}
 }
