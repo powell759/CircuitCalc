f960b2aa8ecd5cdeb014c35b750773b575df0cf1
Maxwell Powell <powell.759@osu.edu>
2016-09-29 15:25:09 -0400

    work on Circuit Class

--- src/Circuit.java
+++ src/Circuit.java
@@ -1,4 +1,17 @@
+import java.util.HashSet;
+import java.util.Set;
 
 public class Circuit {
+	private Set<Node> nodes;
+	private Set<Element> elements;
+	private int current = 0;
+	private int resistance = 0;
+	private int voltage = 0;
+	
+	public Circuit(){
+		this.nodes = new HashSet<Node>();
+		this.elements = new HashSet<Element>();
+	}
+	
 
 }
